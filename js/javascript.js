dd68873aa55dd0149b0ea50cf021873527dd7a8a
rob-macfarlane <remacfarlane29@gmail.com>
2022-12-01 21:30:23 -0800

    Adds functionality for the 0,7, +,/,*,- buttons TODO is write a for loop to create the functionality for the rest of the buttons.

--- js/javascript.js
+++ js/javascript.js
@@ -50,13 +50,67 @@ const four = document.querySelector('.four');
 const three = document.querySelector('.three');
 const two = document.querySelector('.two');
 const one = document.querySelector('.one');
+const zero = document.querySelector('.zero');
 const multiplication = document.querySelector('.multiply');
 const division = document.querySelector('.divide');
 const addition = document.querySelector('.add');
 const subtraction = document.querySelector('.subtract');
 const equals = document.querySelector('.equals');
 
+console.log(zero);
+zero.addEventListener('click', () => {
+    screen.textContent = '0';
+    if (firstInteger === null) {
+        firstInteger = 0;
+    } else if (firstInteger !== null && operator === null) {
+        firstInteger = (firstInteger + '0') - 0;
+        screen.textContent = firstInteger;
+    } else {
+        secondInteger = 0;
+    }
+});
+
+
 console.log(seven);
 seven.addEventListener('click', () => {
     screen.textContent = '7';
+    if (firstInteger === null) {
+        firstInteger = 7;
+    } else if (firstInteger !== null && operator === null) {
+        firstInteger = (firstInteger + '7') - 0;
+        screen.textContent = firstInteger;
+    } else {
+        secondInteger = 7;
+    }
 });
+
+addition.addEventListener('click', () => {
+    screen.textContent = firstInteger + ' +'
+    operator = '+'
+})
+
+equals.addEventListener('click', () => {
+    output = operate(operator, firstInteger, secondInteger);
+    screen.textContent = output;
+    firstInteger = output;
+})
+
+multiplication.addEventListener('click', () => {
+    screen.textContent = firstInteger + ' x'
+    operator = '*'
+})
+
+subtraction.addEventListener('click', () => {
+    screen.textContent = firstInteger + ' -'
+    operator = '-'
+})
+
+division.addEventListener('click', () => {
+    screen.textContent = firstInteger + ' /'
+    operator = '/'
+})
+
+let output = null;
+let operator = null;
+let firstInteger = null;
+let secondInteger = null; 
