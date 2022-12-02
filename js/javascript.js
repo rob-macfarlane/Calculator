9f123521544731b60c5edebfc0a4b88267bcc643
rob-macfarlane <remacfarlane29@gmail.com>
2022-12-01 20:15:47 -0800

    Javascript first event listener added

--- js/javascript.js
+++ js/javascript.js
@@ -38,3 +38,25 @@ function operate(operator, integer1, integer2) {
     return output;
 };
 
+const screen = document.querySelector('.screen');
+console.log(screen);
+
+const nine = document.querySelector('.nine');
+const eight = document.querySelector('.eight');
+const seven = document.querySelector('.seven');
+const six = document.querySelector('.six');
+const five = document.querySelector('.five');
+const four = document.querySelector('.four');
+const three = document.querySelector('.three');
+const two = document.querySelector('.two');
+const one = document.querySelector('.one');
+const multiplication = document.querySelector('.multiply');
+const division = document.querySelector('.divide');
+const addition = document.querySelector('.add');
+const subtraction = document.querySelector('.subtract');
+const equals = document.querySelector('.equals');
+
+console.log(seven);
+seven.addEventListener('click', () => {
+    screen.textContent = '7';
+});
